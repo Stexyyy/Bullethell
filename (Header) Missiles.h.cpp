2655304b9eb91eda5bdf021ca266c7895be63789
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-01-24 14:38:18 -0700

    Create (Header) Missiles.h.cpp

---
+++ (Header) Missiles.h.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "globals.h"
+#include "SFML/Graphics.hpp"
+
+class missile {
+private:
+	int xpos;
+	int ypos;
+	bool alive;
+	sf::Sprite image;
+public:
+	missile(); //default constructor
+	~missile(); //destructor
+	missile(int x, int y, sf::Sprite sprite);
+	missile(int x, int y);
+	void move();
+	void draw(sf::RenderWindow& window);
+	void kill();
+	bool offScreen();
+	void shoot(int x, int y);
+	bool isAlive();
+	bool collide(double x, double y);
+	int getx();
+	int gety();
+
+
+
+
+
+
+};
